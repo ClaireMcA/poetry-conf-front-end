de2b6a352c27e7c00dc3d7b17f469b517dd4cb2d
ClaireMcA <69333056+ClaireMcA@users.noreply.github.com>
2022-11-12 14:05:22 +1100

    router-setup

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -5,8 +5,9 @@ const SignIn = () => {
 
   return (
     <React.Fragment>
-        <Header isAuth="false"/>
-        
+        {/* <Header isAuth="false"/> */}
+        <div></div>
+
     </React.Fragment>
   );
 }
